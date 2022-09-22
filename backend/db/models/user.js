9499a011661a70a96ca1bae2a08083d3be4bf2a2
collodrollo <jxaziz0001@zonemail.clpccd.edu>
2022-09-22 14:40:08 -0700

    added user signup backend endpoint

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -56,7 +56,7 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [4, 30],
           isNotEmail(value) {
-            if (Validator.isEmail) {
+            if (Validator.isEmail(value)) {
               throw new Error("Username shouldn't be email");
             }
           },
