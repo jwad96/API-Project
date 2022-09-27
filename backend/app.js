05b2c65395996d4b79bdfe414ace0ad4910b9207
Jwad <jwad.aziz@gmail.com>
2022-09-27 11:23:28 -0700

    hardcoded id in user seed and isolated Spot.findAll() query

--- backend/app.js
+++ backend/app.js
@@ -32,8 +32,8 @@ app.get('/', async (req, res, next) => {
   } = require('./db/models');
 
   responseObj.users = await User.findAll();
-  responseObj.bookings = await Booking.findAll();
-  // responseObj.spots = await Spot.findAll();
+  // responseObj.bookings = await Booking.findAll();
+  responseObj.spots = await Spot.findAll();
   // responseObj.spotImages = await SpotImage.findAll();
   // responseObj.reviews = await Review.findAll();
   // responseObj.revImages = await ReviewImage.findAll();
