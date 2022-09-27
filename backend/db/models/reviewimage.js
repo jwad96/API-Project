a8e592a735e5f1b1ed448729e6b8bae5c756ac61
Jwad <jwad.aziz@gmail.com>
2022-09-27 10:25:23 -0700

    removed all associations in models

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -9,9 +9,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      ReviewImage.belongsTo(models.Review, {
-        foreignKey: 'reviewId',
-      });
     }
   }
   ReviewImage.init(
--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -9,9 +9,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      SpotImage.belongsTo(models.Spot, {
-        foreignKey: 'spotId',
-      });
     }
   }
   SpotImage.init(
