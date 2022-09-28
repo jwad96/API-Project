48810cd281e0b3119b2300877a0896fa7d273338
Jwad <jwad.aziz@gmail.com>
2022-09-28 11:53:58 -0700

    added firstName, lastName to user signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -25,8 +25,15 @@ const validateSignup = [
 ];
 
 router.post('/', validateSignup, async (req, res) => {
-  const { email, password, username } = req.body;
-  const user = await User.signup({ email, username, password });
+  const { firstName, lastName, email, password, username } = req.body;
+
+  const user = await User.signup({
+    firstName,
+    lastName,
+    email,
+    username,
+    password,
+  });
 
   await setTokenCookie(res, user);
 
