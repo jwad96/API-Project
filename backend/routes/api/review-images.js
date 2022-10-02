db1c786b1bcd9df4f5115ec03c9577a930bb97ef
Jwad <jwad.aziz@gmail.com>
2022-10-01 23:03:49 -0700

    implemented deletion of review image

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,1 +1,23 @@
 const router = require('express').Router();
+const { restoreUser, requireAuth, requireAuthor } = require('../../utils/auth');
+const { ReviewImage, Review } = require('../../db/models');
+
+router.delete('/:imageId', restoreUser, requireAuth, async (req, res, next) => {
+  const reviewImage = await ReviewImage.findByPk(parseInt(req.params.imageId));
+
+  if (!reviewImage) {
+    const err = new Error("Review image couldn't be found");
+    err.status = 404;
+    return next(err);
+  }
+
+  const review = Review.findByPk(reviewImage.reviewId);
+
+  if (req.user.id !== review.userId) {
+    return next(requireAuthor());
+  }
+
+  await reviewImage.destroy();
+
+  res.json({ message: 'Successfully deleted', statusCode: 200 });
+});
