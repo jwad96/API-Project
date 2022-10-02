60afa87d8f129f64c053cea199e365fcb805b0ab
Jwad <jwad.aziz@gmail.com>
2022-10-01 21:15:43 -0700

    changed reference to reviews in avg aggregation

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -338,7 +338,7 @@ router.get('/current', restoreUser, requireAuth, async (req, res, next) => {
         [
           Sequelize.fn(
             'ROUND',
-            Sequelize.fn('AVG', Sequelize.col('spotReviewd.stars')),
+            Sequelize.fn('AVG', Sequelize.col('spotReview.stars')),
             1
           ),
           'avgRating',
