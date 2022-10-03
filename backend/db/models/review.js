4b6f647db270d43864d7fd4dc3d2c0e0e55846d4
Jwad <jwad.aziz@gmail.com>
2022-10-02 19:45:06 -0700

    commented in Review.belongsTO User alias

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
 
       Review.belongsTo(models.User, {
         foreignKey: 'userId',
-        // as: 'User',
+        as: 'User',
       });
     }
   }
